fbe078652f6b592bcbe6625c1e25c1ce51cbba10
HashedDan <georgedanielmangum@gmail.com>
2018-05-01 18:44:06 -0700

    working with graphcool

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import 'tachyons'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = createHttpLink({ uri: 'https://api.graph.cool/simple/v1/cjgocinji2i330107dsev0tho/graphql' });
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    authorization: localStorage.getItem('graphcoolToken') || null,
+  }
+}));
+
+const link = middlewareLink.concat(httpLink)
+
+const client = new ApolloClient({ link });
+
+ReactDOM.render((
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={App} />
+          {/* <Route exact path='/create' component={CreatePost} /> */}
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
+    ), 
+    document.getElementById('root'));
 registerServiceWorker();
